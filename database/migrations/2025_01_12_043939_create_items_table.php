c48e191baead0be473f1e301d619c51f9c8246ea
Paulo Tenório <paulorltenorio@gmail.com>
2025-01-12 02:09:52 -0300

    incluído os campos: 'name', 'description' e 'price', na função 'up',  para criar a tabela no migration 'create_items_table' após uso do php artisan migrate

--- database/migrations/2025_01_12_043939_create_items_table.php
+++ database/migrations/2025_01_12_043939_create_items_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('items', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('items', function (Blueprint $table) {
+        $table->id(); // ID automático
+        $table->string('name'); // Nome do item
+        $table->text('description')->nullable(); // Descrição (opcional)
+        $table->decimal('price', 8, 2)->nullable(); // Preço (opcional)
+        $table->timestamps(); // Cria as colunas created_at e updated_at
+    });
+}
 
     /**
      * Reverse the migrations.
